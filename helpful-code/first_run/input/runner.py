9326919f94e16fbdb92116ba05c0c91ca91ff89f
Alyona <alonakosobokova@gmail.com>
2025-10-11 19:26:41 -0700

    Update runner.py

--- helpful-code/first_run/input/runner.py
+++ helpful-code/first_run/input/runner.py
@@ -1,1 +1,21 @@
+from pdgworkflow import WorkflowManager 
 
+config = {
+    "dir_input": " put your dir path here/input",
+    "ext_input": ".gpkg",
+    "z_range": (0, 2),            # keep it small for a faster test run
+    "overwrite": True,   
+  
+# Optional: disable features if you want a smoke test
+  
+    # "enable_3dtiles": False,
+    # "enable_raster_parents": False,
+    # "enable_web_tiles_parents": False,
+    # "generate_wmtsCapabilities": False,
+  
+}
+
+workflow = WorkflowManager(config)
+workflow.run_workflow()
+
+print("Finished the first run.")
